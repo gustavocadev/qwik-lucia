bfc9e0ce32ac7e79a5742e6813f3084ffaa25d65
gustavocadev <gustavocadev@gmail.com>
2024-02-22 09:32:29 -0500

    Add JSdoc

--- src/utils/hashPasword.ts
+++ src/utils/hashPasword.ts
@@ -1,5 +1,10 @@
 import { Argon2id } from 'oslo/password';
 
+/**
+ * Hashes the password using Argon2id which is provided by the Oslo library
+ * @param password - The password to hash
+ * @returns The hashed password
+ */
 export const hashPassword = async (password: string) => {
   return new Argon2id().hash(password);
 };
--- src/utils/verifyPassword.ts
+++ src/utils/verifyPassword.ts
@@ -1,5 +1,28 @@
 import { Argon2id } from 'oslo/password';
 
+/**
+ * Verifies the password using Argon2id which is provided by the Oslo library
+ * @param passwordHash - The hashed password
+ * @param password - The password to verify
+ * @returns Whether the password is valid
+ * @example
+ * ```ts
+ * const passwordHash = await hashPassword('password');
+ * const isValid = await verifyPassword(passwordHash, 'password');
+ * if (!isValid) {
+ *  console.log('Invalid password');
+ * }
+ *```
+ * @example
+ * ```ts
+ * const passwordHash = await hashPassword('password');
+ * const isValid = await verifyPassword(passwordHash, 'password');
+ * if (isValid) {
+ *  console.log('Valid password');
+ * }
+ * ```
+ */
+
 export const verifyPassword = async (
   passwordHash: string,
   password: string
